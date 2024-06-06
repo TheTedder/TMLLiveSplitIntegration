3758843178f1b495d028431ea51bfe8150bff802
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-06-06 14:29:25 -0400

    Squashed commit of the following:
    commit 1ae26393ca45300edb2540d73241a019efa3bfb0
    Author: Ted Wollman <25165500+TheTedder@users.noreply.github.com>
    Date:   Thu Jun 6 14:29:13 2024 -0400
    
        Add methods for sending messages.
    
    commit e71029c2af8d2dde7d63e9d891ee1ac7c22ee71e
    Author: Ted Wollman <25165500+TheTedder@users.noreply.github.com>
    Date:   Thu Jun 6 14:28:15 2024 -0400
    
        Manage client lifetime.
    
    commit 17d9de978854f1e06913521724ca2997892124f8
    Author: Ted Wollman <25165500+TheTedder@users.noreply.github.com>
    Date:   Thu Jun 6 14:27:20 2024 -0400
    
        Format document.

--- LiveSplitIntegration.cs
+++ LiveSplitIntegration.cs
@@ -1,15 +1,108 @@
+using LiveSplitInterop.Clients;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Terraria.ModLoader;
 
 namespace LiveSplitIntegration
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class LiveSplitIntegration : Mod
-	{
+    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+    public class LiveSplitIntegration : Mod
+    {
+        private static NamedPipeCommandClient Client;
+        private static readonly object ClientLock = new();
+        private static CancellationTokenSource connectSource;
 
-	}
+        private static async Task Connect()
+        {
+            Client = new();
+            await Client.ConnectAsync(connectSource.Token);
+        }
+
+        public override void Load()
+        {
+            connectSource = new();
+            Task.Run(Connect);
+        }
+
+        public override void Unload()
+        {
+            connectSource.Cancel();
+            connectSource.Dispose();
+            Client?.Dispose();
+        }
+
+        internal static bool TrySendLsMsg(Action<NamedPipeCommandClient> msgAction)
+        {
+            lock (ClientLock)
+            {
+                if (!Client?.IsConnected ?? false)
+                {
+                    return false;
+                }
+
+                try
+                {
+                    msgAction(Client);
+                    return true;
+                }
+                catch (IOException)
+                {
+                    //TODO: log
+                }
+                catch (ObjectDisposedException)
+                {
+                    return false;
+                }
+
+                if (!connectSource.IsCancellationRequested)
+                {
+                    Client.Dispose();
+                    Task.Run(Connect);
+                }
+
+                return false;
+            }
+        }
+
+        internal static bool TrySendLsMsg<T>(Func<NamedPipeCommandClient, T> msgAction, out T res)
+        {
+            lock (ClientLock)
+            {
+                if (!Client?.IsConnected ?? false)
+                {
+                    res = default;
+                    return false;
+                }
+
+                try
+                {
+                    res = msgAction(Client);
+                    return true;
+                }
+                catch (IOException)
+                {
+                    //TODO: log
+                }
+                catch (ObjectDisposedException)
+                {
+                    res = default;
+                    return false;
+                }
+
+                if (!connectSource.IsCancellationRequested)
+                {
+                    Client.Dispose();
+                    Task.Run(Connect);
+                }
+
+                res = default;
+                return false;
+            }
+        }
+    }
 }
