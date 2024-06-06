28784b6fc2a812cf976f11763da11077f4513a4f
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-06-06 14:30:05 -0400

    Squashed commit of the following:
    commit 59854ee757f2f16b990e8f1071a56ccceb022768
    Author: Ted Wollman <25165500+TheTedder@users.noreply.github.com>
    Date:   Thu Jun 6 14:29:56 2024 -0400
    
        Create AutoStart.cs

---
+++ AutoStart.cs
@@ -0,0 +1,18 @@
+﻿using LiveSplitInterop.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace LiveSplitIntegration
+{
+    public class AutoStart : ModPlayer
+    {
+        public override void OnEnterWorld()
+        {
+            LiveSplitIntegration.TrySendLsMsg(Client => Client.Start());
+        }
+    }
+}
