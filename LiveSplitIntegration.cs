3d2655d38fe40d47c13b81131a28628f3ab263ef
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-06-10 13:16:14 -0400

    Remove default comment.

--- LiveSplitIntegration.cs
+++ LiveSplitIntegration.cs
@@ -10,7 +10,6 @@ using Terraria.ModLoader;
 
 namespace LiveSplitIntegration
 {
-    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
     public class LiveSplitIntegration : Mod
     {
         private static NamedPipeCommandClient Client;
