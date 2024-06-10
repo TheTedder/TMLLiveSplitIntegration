8e83dcd251b75a39d43f6cfc4c325aa0e30cca09
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-06-10 14:56:44 -0400

    Rename Downed and use a property.

--- BossStatus.cs
+++ BossStatus.cs
@@ -9,17 +9,18 @@ namespace LiveSplitIntegration
 {
     internal class BossStatus(bool initialState = false)
     {
-        public Func<bool> Downed { get; init; }
+        public Func<bool> IsDowned { private get; init; }
+        public bool Downed { get; private set; } = initialState;
         public bool DownedLast { get; private set; } = initialState;
         public Func<LiveSplitIntegrationConfig, bool> Enabled { get; init; }
         private readonly LiveSplitIntegrationConfig config = ModContent.GetInstance<LiveSplitIntegrationConfig>();
 
         public bool Update()
         {
-            bool downed = Downed();
-            bool split = downed && !DownedLast;
-            DownedLast = downed;
-            return split && Enabled(config);
+            Downed = IsDowned();
+            bool split = Downed && !DownedLast && Enabled(config);
+            DownedLast = Downed;
+            return split;
         }
     }
 }
